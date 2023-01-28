d073a2daf90c4a0fb591af778263fbf297214cf7
nayrao <48581744+nayrao@users.noreply.github.com>
2023-01-28 23:47:58 +0530

    Update FeignClientExample.java

--- USER-API/src/main/java/com/example/client/FeignClientExample.java
+++ USER-API/src/main/java/com/example/client/FeignClientExample.java
@@ -3,7 +3,7 @@ package com.example.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-
+// here is the communication between two apis
 @FeignClient(name = "WELCOME-API")
 public interface FeignClientExample {
 
